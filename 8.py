f4bdcda55c691b87894b69fd7332e39636e05f72
Karolina060 <131521991+Karolina060@users.noreply.github.com>
2023-05-17 09:54:09 +0200

    Add files via upload

--- 8.py
+++ 8.py
@@ -1,35 +1,35 @@
 #derivace
-def forward_derivate(f, x0, k):
+def forward(f, x0, k):
     return (f(x0+k) - f(x0))/k
 
-def backward_derivate(f, x0, k):
+def backward(f, x0, k):
     return (f(x0) - f(x0-k))/k
 
-def central_derivate(f, x0, k):
+def central(f, x0, k):
     return (f(x0+k) - f(x0-k))/(2*k)
 
-def forward_derivate_adaptive(f, x0, adaptive_k):
+def forward_a(f, x0, adaptive_k):
     return (f(x0+adaptive_k) - f(x0))/adaptive_k
 
-def backward_derivate_adaptive(f, x0, adaptive_k):
+def backward_a(f, x0, adaptive_k):
     return (f(x0) - f(x0-adaptive_k))/adaptive_k
 
-def central_derivate_adaptive(f, x0, adaptive_k):
+def central_a(f, x0, adaptive_k):
     return (f(x0+adaptive_k) - f(x0-adaptive_k))/(2*adaptive_k)
 
 #funkce
 f = lambda x: x**4 + 2*x**3 - 6
 x0 = 3
 k = 0.1
-adaptive_k = float(input("Zadej adaptivní krok (nejlépe od 0 do 1): "))
+adaptive_k = float(input("Zadej krok (od 0 do 1): "))
 print("\n")
 
-forw_de = forward_derivate(f, x0, k)
-back_de = backward_derivate(f, x0, k)
-cent_de = central_derivate(f, x0, k)
-forw= forward_derivate_adaptive(f, x0, adaptive_k)
-back = backward_derivate_adaptive(f, x0, adaptive_k)
-cent = central_derivate_adaptive(f, x0, adaptive_k)
+forw_de = forward(f, x0, k)
+back_de = backward(f, x0, k)
+cent_de = central(f, x0, k)
+forw= forward_a(f, x0, adaptive_k)
+back = backward_a(f, x0, adaptive_k)
+cent = central_a(f, x0, adaptive_k)
 
 #výsledek
 print(f"Dopředná bez kroků: {forw_de}, dopředná s krokem: {forw}, rozdíl = {abs(forw_de- forw)}")
